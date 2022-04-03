ad369f62c492c9c9b7dbbff1399ef96d0a6ec15a
ManikandanKumarkp <manikandankumarkp@gmail.com>
2022-04-03 20:37:38 +0530

    1 file modified

--- src/app/Employee/employee.component.ts
+++ src/app/Employee/employee.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'emp',
-  templateUrl:'./employee.component.html',
+  templateUrl:`./employee.component.html`,
   styles: [`p{ font-style:san; } `]
 })
 export class EmployeeComponent { 
