58546ca07cdd7df21d62da5fd7006887b653597d
ManikandanKumarkp <manikandankumarkp@gmail.com>
2022-04-03 16:21:42 +0530

    1 file modified

--- src/app/Employee/employee.component.ts
+++ src/app/Employee/employee.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'emp',
   templateUrl:'./employee.component.html',
-  styles: [`p{ font-style:san; } `],
+  styles: [`p{ font-style:san; } `]
 })
 export class EmployeeComponent { 
   code:number= 34;
