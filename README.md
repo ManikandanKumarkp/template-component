f49c60122c8c292c9bee3850d9b0f0bbfdf5c5da
ManikandanKumarkp <manikandankumarkp@gmail.com>
2022-04-03 14:12:29 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-az5vwj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-az5vwj)
---
+++ src/app/Employee/employee.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'emp',
+  templateUrl: `./employee.component.html`,
+  styles: [`p{ font-style:san; } `],
+})
+export class EmployeeComponent { 
+
+}
