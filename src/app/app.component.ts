989180df3de3fdbb51bbb90193e3581eb10ce90e
ManikandanKumarkp <manikandankumarkp@gmail.com>
2022-04-03 20:40:11 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,10 @@
-<p>Profile</p>
-<table>
-  <thead>
-<th> Code  </th>
-<th> Name </th>
-<th> Gender </th>
-<th> Dob </th>
-<th>Age </th>
-  </thead>
-</tr>
-<tr>
-<td>{{code }}</td>
-<td>{{name }}</td>
-<td>{{gender}}</td>
-</tr>
-</table>
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = " Employee";
+}
